f2f08abd9df5212ee6a646d038fc7e068348d4bb
MS-Marcks <gersonramos46@gmail.com>
2021-09-23 10:30:12 -0600

    se agrego los datos del docker

--- TargetServer/src/db/index.js
+++ TargetServer/src/db/index.js
@@ -1,11 +1,9 @@
-
 const users = [
     {
         "id": "1",
         "name": "test",
         "email": "test@test.com",
-        "password": "1234",
-        "amount": 1000
+        "password": "1234"
     }
 ];
 
